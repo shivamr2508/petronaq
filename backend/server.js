713a65d2b7a6569ac8963a379cef482d82c2dc64
sumit <sumitsinghr8424@gmail.com>
2026-03-20 19:10:43 +0530

    downgrade express to v4

--- backend/server.js
+++ backend/server.js
@@ -77,7 +77,7 @@ const path = require("path");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
-  app.get("/*", (req, res) =>
+  app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "../Frontend", "dist", "index.html"))
   );
 }
